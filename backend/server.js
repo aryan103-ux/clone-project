d73a306df2280069aafc6a518a080d0248a76e50
Aryan Dhiman <aryandhiman078@gmail.com>
2026-03-29 11:13:04 +0530

    autosetup temp

--- backend/server.js
+++ backend/server.js
@@ -59,5 +59,15 @@ app.listen(PORT, async () => {
   console.log(`\n🚀 Amazon Clone running at http://localhost:${PORT}`);
   console.log(`   Environment: ${process.env.NODE_ENV}\n`);
 
+    // 🚨 AUTO SETUP (TEMP)
+  if (process.env.NODE_ENV === 'production') {
+    console.log("⚙️ Running DB setup...");
 
+    try {
+      await require('./db/setup');
+      console.log("✅ DB setup finished");
+    } catch (err) {
+      console.error("❌ DB setup failed:", err);
+    }
+  }
 });
